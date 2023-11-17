a9b67c962a47d561bbad233b4a91f6b5a5be3a99
Henrique de Souza <henriquezkagz@gmail.com>
2023-11-16 21:17:42 -0300

    Padaria Ortega
    PHP TESTE

---
+++ teste.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = "Henrique de Souza";
+$cidade = "São Paulo";
+$telefone = "11 98647-4161";
+$endereço = "Rua Maria das Dores, 635";
+$email = "teste@gmail.com";
+$CPF = "000.000.000-00";
+$bairro = "Itaquera";
+
+echo "Olá $nome, bem vindo à $cidade, seu novo endereço é $endereço, $bairro \n";
+
+
+
+echo "Confirme suas informações: CPF:$CPF, EMAIL:$email, TELEFONE:$telefone";
+
+?>
